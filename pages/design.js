b946950b18ba88b02250b030ffe262046efb0ca0
Archie Baer <archie@abaer.dev>
2020-01-20 22:41:57 +0000

    Homepage and Design page changes

--- pages/design.js
+++ pages/design.js
@@ -1,13 +1,12 @@
 import Page from "../layout/Page";
 import withAuth from "../util/withAuth";
 
-const testPhrase = "The quick brown fox jumped over the lazy dog.";
-const fontWeights = [300, 400, 500, 700, 900];
-
 export default withAuth(props => (
   <Page header padding user={props.user}>
-    <p>{testPhrase.toUpperCase()}</p>
-    <p>{testPhrase.toLowerCase()}</p>
-    <p>{fontWeights.map((w) => <span style={{fontWeight: w}}>{w} </span>)}</p>
+    <p>THE QUICK BROWN FOX JUMPED OVER THE LAZY DOG.</p>
+    <p>the quick brown fox jumped over the lazy dog.</p>
+    <p>{[
+        300, 400, 500, 700, 900
+    ].map((w) => <span style={{fontWeight: w}}>{w} </span>)}</p>
   </Page>
 ));
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
 import Page from "../layout/Page";
 import withAuth from "../util/withAuth";
+import {useState} from "react";
 
-export default withAuth(props => (
-  <Page header padding user={props.user}>
-    <p>THE QUICK BROWN FOX JUMPED OVER THE LAZY DOG.</p>
-  </Page>
-));
+export default withAuth(props => {
+  const [h] = useState(new Date().getHours());
+
+  return (
+    <Page header padding user={props.user}>
+      <h1>Good {h < 3 ? "Night" : h < 12 ? "Morning" : h < 18 ? "Afternoon" : "Evening"}, Archie!</h1>
+    </Page>
+  );
+});
