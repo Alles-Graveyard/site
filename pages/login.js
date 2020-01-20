3a961f427a0ac199b7d12436dfabaa8cde135267
Archie Baer <archie@abaer.dev>
2020-01-20 20:31:40 +0000

    Login page

--- pages/login.js
+++ pages/login.js
@@ -1,3 +1,34 @@
-export default () => (
-    <p>test</p>
-);
+import axios from "axios";
+import config from "../config";
+import {useState} from "react";
+import Cookies from "js-cookie";
+import router, {withRouter} from "next/router";
+
+const LoginForm = props => {
+    const [formData, setFormData] = useState({username: "", password: ""});
+    return (
+        <form onSubmit={(e) => {
+            e.preventDefault();
+            axios.post(`${config.apiUrl}/login`, {
+                username: formData.username,
+                password: formData.password
+            }).then((res) => {
+                Cookies.set("sessionToken", res.data.token);
+                const redirectUrl = props.router.query.redirect;
+                router.push(redirectUrl ? redirectUrl : "/");
+            }).catch((err) => {
+                console.log(err);
+            });
+        }}>
+            <input onChange={(e) => {
+                setFormData({...formData, username: e.target.value.trim().toLowerCase()});
+            }} placeholder="Username" />
+            <input onChange={(e) => {
+                setFormData({...formData, password: e.target.value});
+            }} placeholder="Password" type="password" />
+            <button>Sign in</button>
+        </form>
+    );
+};
+
+export default withRouter(LoginForm);
