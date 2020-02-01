5e775f6f8375587c2cc57aac36860fddc7cb7564
Archie Baer <archie@abaer.dev>
2020-02-01 22:49:55 +0000

    Simplified User in header

--- layout/Header.js
+++ layout/Header.js
@@ -8,13 +8,7 @@ export default props => (
         </div>
 
         <Link href="/me">
-            <div className="user">
-                <div className="userInner">
-                    <p className="nickname">{props.user.nickname}</p>
-                    <p className="username">@{props.user.username}</p>
-                </div>
-                <img className="profilePicture" src="https://pbs.twimg.com/profile_images/1180922399790944257/3lC1YOEY_400x400.png" />
-            </div>
+            <img className="profilePicture" src="https://pbs.twimg.com/profile_images/1180922399790944257/3lC1YOEY_400x400.png" />
         </Link>
 
         <style jsx>{`
@@ -43,39 +37,13 @@ export default props => (
                 font-weight: 700;
             }
 
-            .user {
-                border-radius: 100px;
-                padding: 5px 5px 5px 15px;
-                cursor: pointer;
-                vertical-align: middle;
-                display: flex;
-                border: solid 1px ${theme.borderGrey};
-            }
-
-            .user img {
+            .profilePicture {
                 border: solid 1px ${theme.borderGrey};
                 border-radius: 50%;
                 height: 2.5em;
                 width: 2.5em;
-                vertical-align: middle;
                 margin: auto 0 auto 5px;
-            }
-
-            .user .userInner {
-                text-align: right;
-            }
-
-            .user p {
-                margin: 0;
-            }
-
-            .user .nickname {
-                font-size: 20px;
-            }
-
-            .user .username {
-                font-size: 10px;
-                font-weight: 300;
+                cursor: pointer;
             }
         `}</style>
     </header>
