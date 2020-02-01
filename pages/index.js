00161bce1830bf40c5efe94a0da85c0239bf565c
Archie Baer <archie@abaer.dev>
2020-02-01 23:24:39 +0000

    Improvements to Account Page

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,40 @@
 import Page from "../layout/Page";
 import withAuth from "../util/withAuth";
 import theme from "../theme";
+import {useState, useEffect} from "react";
+import monYearDate from "../util/monYearDate";
 
 export default withAuth(props => {
+  const [plusDate, setPlusDate] = useState();
+
+  useEffect(() => {
+    if (props.user.plus.status === "active") {
+      //Format Alles+ "since" date
+      setPlusDate(monYearDate(new Date(props.user.plus.since)));
+    }
+  }, []);
+
   return (
     <Page title="My Account" header style={{background: theme.greyF}} user={props.user}>
 
       <section className="user">
         <img className="profilePicture" src="https://pbs.twimg.com/profile_images/1180922399790944257/3lC1YOEY_400x400.png" />
-        <h1 className="name">{props.user.name}</h1>
+        <h1 className="name">{props.user.name}{props.user.plus.status === "active" ? <sup>+</sup> : <></>}</h1>
         <h2 className="username">@{props.user.username}</h2>
+
+        <div className="quickInfo">
+
+          <div>
+            <i className="fas fa-coins"></i>
+            <p><b>{props.user.au}</b>au</p>
+          </div>
+
+          <div>
+            <i className="fas fa-plus-circle"></i>
+            <p>{props.user.plus.status === "active" ? (plusDate ? `Since ${plusDate}` : "Active") : props.user.plus.status === "expired" ? "Expired" : "Try Alles+"}</p>
+          </div>
+
+        </div>
       </section>
 
       <style jsx>{`
@@ -22,7 +47,6 @@ export default withAuth(props => {
           padding-top: 0;
           box-sizing: border-box;
           text-align: center;
-          overflow: unset hidden;
         }
 
         .profilePicture {
@@ -46,9 +70,34 @@ export default withAuth(props => {
 
         h2.username {
           font-size: 15px;
-          font-weight: 300;
+          font-weight: 400;
           margin: 0;
           margin-bottom: 10px;
+          color: ${theme.accent};
+        }
+
+        .quickInfo {
+          display: flex;
+          flex-wrap: wrap;
+          justify-content: center;
+        }
+
+        .quickInfo div {
+          min-width: 150px;
+          padding: 10px;
+        }
+
+        .quickInfo div:hover {
+          background: #f0f0f080;
+        }
+
+        .quickInfo div i {
+          font-size: 20px;
+          margin-bottom: 10px;
+        }
+
+        .quickInfo div p {
+          margin: 0;
         }
       `}</style>
     </Page>
