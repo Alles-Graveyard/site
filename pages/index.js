704bf80e759ed27a3d2a15d57e6bd5679bbc5993
Archie Baer <archie@abaer.dev>
2020-02-02 02:50:32 +0000

    Homepage placeholder

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import Page from "../layout/Page";
 import withAuth from "../util/withAuth";
-import theme from "../theme";
+import Link from "next/link";
 
 export default withAuth(props => {
   return (
     <Page header user={props.user}>
+      <p>This page isn't done yet. Go to <Link href="/me"><a>your account page</a></Link> to manage settings.</p>
     </Page>
   );
 });
