1336d6ac05116c957959cf20c122da8e03eb87ca
Archie Baer <archie@abaer.dev>
2020-03-14 15:45:42 +0000

    No user error and bottom arrow set

--- pages/people.js
+++ pages/people.js
@@ -7,15 +7,14 @@ import axios from "axios";
 import {withRouter} from "next/router";
 
 const people = props => {
+	const backwardsBtn = props.users.length > 0 && !props.firstPage ? props.users[0].username : null;
+	const forwardsBtn = props.users.length > 0 && !props.lastPage ? props.users[props.users.length - 1].username : null;
 	return (
 		<Page header user={props.user} title="Users">
-			<NavArrows
-				before={
-					props.users.length > 0 && !props.firstPage ? props.users[0].username : null
-				}
-				after={
-					props.users.length > 0 && !props.lastPage ? props.users[props.users.length - 1].username : null
-				}
+			{props.users.length > 0 ? (
+				<><NavArrows
+				before={backwardsBtn}
+				after={forwardsBtn}
 			/>
 			{props.users.map(u => (
 				<Link href="/u/[id]" as={`/u/${u.username}`} key={u.id}>
@@ -31,6 +30,16 @@ const people = props => {
 					</a>
 				</Link>
 			))}
+			<NavArrows
+				before={backwardsBtn}
+				after={forwardsBtn}
+			/></>
+			) : (
+				<div style={{textAlign: "center"}}>
+					<h1>Error</h1>
+					<p>No users were found.</p>
+				</div>
+			)}
 
 			<style jsx>{`
 				.user {
