758332330563c5ad1e586ff58ecf1d1434f6321e
Archie Baer <archie@abaer.dev>
2020-03-23 11:35:01 +0000

    Fixed error when no personal au accounts

--- pages/me.js
+++ pages/me.js
@@ -146,8 +146,8 @@ const homepage = props => {
 	};
 
 	//Au Balance
-	const auBalance = props.auAccounts[0].balance;
-	const auDisplay = formatAu(auBalance);
+	const auBalance = props.auAccounts.length > 0 ? props.auAccounts[0].balance : null;
+	const auDisplay = auBalance !== null ? formatAu(auBalance) : null;
 
 	return (
 		<Page title="My Account" header banner={banner} user={props.user}>
