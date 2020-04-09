a6cffddfddc610329a75ebba03420242c15686d9
Archie Baer <archie@abaer.dev>
2020-04-09 01:20:26 +0100

    Removed basic-profile scope

--- config.js
+++ config.js
@@ -4,7 +4,6 @@ export default {
 	apiUrl: `${dev ? "http://localhost" : "https://alles.cx"}/api/v1`,
 	dev,
 	scopes: {
-		"basic-profile": "View basic profile information",
 		"team-list": "Know what teams you are in"
 	},
 	inputBounds: {
