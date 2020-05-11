8b87cbef4c9c958679dff15b71366435930bcde0
Archie Baer <archie@abaer.dev>
2020-05-11 02:32:35 +0100

    Updated console message to use discord server url

--- util/consoleWarning.js
+++ util/consoleWarning.js
@@ -9,7 +9,7 @@ export default () => {
 %c
 This is the developer console, which is usually used for testing the site. However, there are some dangerous commands that, when entered here, may reveal tokens that can be used to access your account.
 
-Unless you know what you're doing, you should close this, and if an Alles user told you to enter something here, you should report them. Even Alles staff members shouldn't need you to do anything. However, if you do know what you're doing, you should join our community of developers: https://dev.alles.cx
+Unless you know what you're doing, you should close this, and if an Alles user told you to enter something here, you should report them. Even Alles staff members shouldn't need you to do anything. However, if you do know what you're doing, you should join our community of developers: https://discord.gg/aybNznh
 
 `,
 		`color: ${color}; font-size: 40px;`,
