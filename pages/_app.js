023fa5f3fa8fa6d644b9ae75ed6e0a9c5aeb5c62
Archie Baer <archie@abaer.dev>
2020-05-12 15:32:08 +0100

    Updated reactants

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import {withTheme} from "@reactants/ui";
+import "@reactants/ui/dist/index.css";
 import "@reactants/ui/dist/global.css";
 
 export default ({Component, pageProps}) => <Component {...pageProps} />;
