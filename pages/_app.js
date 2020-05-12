59366b19017ddc86b6251bad56653918ba981a49
Archie Baer <archie@abaer.dev>
2020-05-12 18:09:41 +0100

    nprogress

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
+import Router from "next/router";
+import NProgress from "nprogress";
 import {withTheme} from "@reactants/ui";
 import "@reactants/ui/dist/index.css";
 import "@reactants/ui/dist/global.css";
+import "../nprogress.css";
 
+// Progress Bar
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
+
+// Export
 export default withTheme(({Component, pageProps}) => (
 	<Component {...pageProps} />
 ));
