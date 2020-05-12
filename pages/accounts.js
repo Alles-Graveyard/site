ffba7b1fb89cf8166518a5102ccf3dac4f090ad9
Archie Baer <archie@abaer.dev>
2020-05-12 17:06:34 +0100

    Improved accounts page

--- pages/accounts.js
+++ pages/accounts.js
@@ -3,6 +3,7 @@ import withAuth from "../util/withAuth";
 import config from "../config";
 import axios from "axios";
 import WideUser from "../components/WideUser";
+import {Box} from "@reactants/ui";
 
 const page = props => {
 	const switchUser = id => {
@@ -31,53 +32,47 @@ const page = props => {
 				}
 			]}
 		>
-			<section>
-				<h1>My Accounts</h1>
-				<p>
-					Sign in to a related account. We allow each person to have one primary
-					account, then they can create other secondary accounts.
-				</p>
-				<h3>Primary Account</h3>
-				<WideUser
-					user={props.accounts.primary ? props.accounts.primary : props.user}
-					onClick={() =>
-						switchUser(
-							props.accounts.primary ? props.accounts.primary.id : props.user.id
-						)
-					}
-					style={{
-						cursor: "pointer"
-					}}
-				/>
+			<Box>
+				<Box.Header>My Accounts</Box.Header>
+				<Box.Content>
+					<p>
+						Sign in to a related account. We allow each person to have one
+						primary account, then they can create other secondary accounts.
+					</p>
+					<h3>Primary Account</h3>
+					<WideUser
+						user={props.accounts.primary ? props.accounts.primary : props.user}
+						onClick={() =>
+							switchUser(
+								props.accounts.primary
+									? props.accounts.primary.id
+									: props.user.id
+							)
+						}
+						style={{
+							cursor: "pointer"
+						}}
+					/>
 
-				<h3>Secondary Accounts</h3>
-				{props.accounts.secondaries.length > 0 ? (
-					props.accounts.secondaries.map(u => (
-						<WideUser
-							user={u}
-							key={u.id}
-							onClick={() => switchUser(u.id)}
-							style={{
-								cursor: "pointer"
-							}}
-						/>
-					))
-				) : (
-					<p>It seems you have no secondary accounts.</p>
-				)}
-			</section>
+					<h3>Secondary Accounts</h3>
+					{props.accounts.secondaries.length > 0 ? (
+						props.accounts.secondaries.map(u => (
+							<WideUser
+								user={u}
+								key={u.id}
+								onClick={() => switchUser(u.id)}
+								style={{
+									cursor: "pointer"
+								}}
+							/>
+						))
+					) : (
+						<p>It seems you have no secondary accounts.</p>
+					)}
+				</Box.Content>
+			</Box>
 
 			<style jsx>{`
-				section {
-					background: white;
-					width: 600px;
-					max-width: 100%;
-					margin: 20px auto;
-					border-radius: 10px;
-					padding: 20px;
-					box-sizing: border-box;
-				}
-
 				h3 {
 					margin-top: 50px;
 				}
