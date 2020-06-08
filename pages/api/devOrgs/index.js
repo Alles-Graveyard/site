4b480f26570274f3b04c825aec2029b9c5beacb8
Archie Baer <archie@abaer.dev>
2020-06-08 23:37:31 +0100

    Dev Org list

---
+++ pages/api/devOrgs/index.js
@@ -0,0 +1,35 @@
+import sessionAuth from "../../../util/sessionAuth";
+import db from "../../../util/db";
+
+export default async (req, res) => {
+	const {user} = await sessionAuth(req.headers.authorization);
+	if (!user) return res.status(401).json({err: "invalidSession"});
+
+	// Get Memberships
+	const memberships = await db.DevOrgMember.findAll({
+		where: {
+			userId: user.id,
+			accepted: true
+		},
+		order: ["createdAt"]
+	});
+
+	// Response
+	res.json({
+		orgs: await Promise.all(
+			memberships.map(async m => {
+				const org = await db.DevOrg.findOne({
+					where: {
+						id: m.orgId
+					}
+				});
+
+				return {
+					id: org.id,
+					name: org.name,
+					color: org.color
+				};
+			})
+		)
+	});
+};
--- pages/dev/index.js
+++ pages/dev/index.js
@@ -1,8 +1,8 @@
 import Page from "../../components/Page";
 import withAuth from "../../util/withAuth";
 import axios from "axios";
-import Post from "../../components/Post";
-import {Spacer} from "@reactants/ui";
+import {Box, Spacer} from "@reactants/ui";
+import Link from "next/link";
 
 const page = props => {
 	return (
@@ -15,16 +15,30 @@ const page = props => {
 				}
 			]}
 		>
-			{props.mentions.length > 0 ? (
-				props.mentions.map(p => (
-					<React.Fragment key={p.slug}>
+			{props.orgs.length > 0 ? (
+				props.orgs.map(org => (
+					<React.Fragment key={org.id}>
 						<Spacer y={2} />
 
-						<Post
-							data={p}
-							self={props.user.id === p.author.id}
-							sessionToken={props.user.sessionToken}
-						/>
+						<Link href="/dev/org/[id]" as={`/dev/org/${org.id}`}>
+							<a className="org">
+								<Box>
+									<Box.Content
+										style={{
+											display: "flex"
+										}}
+									>
+										<div
+											className="circle"
+											style={{
+												backgroundColor: `#${org.color}`
+											}}
+										></div>
+										<h1>{org.name}</h1>
+									</Box.Content>
+								</Box>
+							</a>
+						</Link>
 					</React.Fragment>
 				))
 			) : (
@@ -33,23 +47,37 @@ const page = props => {
 						textAlign: "center"
 					}}
 				>
-					No recent mentions.
+					You're not in any orgs just yet!
 				</p>
 			)}
+
+			<style jsx>{`
+				.circle {
+					height: 30px;
+					width: 30px;
+					border-radius: 50%;
+					margin: auto 0;
+					margin-right: 20px;
+				}
+
+				.org h1 {
+					margin: 0;
+					font-weight: 500;
+					font-size: 30px;
+				}
+			`}</style>
 		</Page>
 	);
 };
 
 page.getInitialProps = async ctx => {
-	return {
-		mentions: (
-			await axios.get(`${process.env.NEXT_PUBLIC_APIURL}/mentions`, {
-				headers: {
-					authorization: ctx.user.sessionToken
-				}
-			})
-		).data.mentions
-	};
+	return (
+		await axios.get(`${process.env.NEXT_PUBLIC_APIURL}/devOrgs`, {
+			headers: {
+				authorization: ctx.user.sessionToken
+			}
+		})
+	).data;
 };
 
 export default withAuth(page);
