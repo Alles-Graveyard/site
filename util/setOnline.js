7d296bda6fe5630302684123ee5d6b2bd8e0ee4a
Archie Baer <archie@abaer.dev>
2020-06-11 02:12:02 +0100

    Formatting

--- util/setOnline.js
+++ util/setOnline.js
@@ -1,15 +1,17 @@
 import axios from "axios";
 
-export default (sessionToken) => {
-    const setOnline = () => {
-        axios.get("https://online.alles.cx", {
-            headers: {
-                authorization: sessionToken
-            }
-        }).catch(() => {});
-    };
+export default sessionToken => {
+	const setOnline = () => {
+		axios
+			.get("https://online.alles.cx", {
+				headers: {
+					authorization: sessionToken
+				}
+			})
+			.catch(() => {});
+	};
 
-    setOnline();
-    const interval = setInterval(setOnline, 10000);
-    return () => clearInterval(interval);
+	setOnline();
+	const interval = setInterval(setOnline, 10000);
+	return () => clearInterval(interval);
 };
