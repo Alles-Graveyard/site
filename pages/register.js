85a00a4057eefb776b8cdd0bdd6512c5ffe5e544
Archie Baer <archie@abaer.dev>
2020-06-13 14:03:43 +0100

    Disabled registration page in beta

--- pages/register.js
+++ pages/register.js
@@ -47,146 +47,156 @@ export default withAuth(props => {
 			<Page title="Join Alles!" width="375px">
 				<h2 style={{textAlign: "center"}}>Join Alles!</h2>
 
-				<Box
-					as="form"
-					onSubmit={e => {
-						e.preventDefault();
-						setError();
+				{process.env.NEXT_PUBLIC_MODE === "beta" ? (
+					<p
+						style={{
+							textAlign: "center"
+						}}
+					>
+						You can't create an account on the beta site.
+					</p>
+				) : (
+					<Box
+						as="form"
+						onSubmit={e => {
+							e.preventDefault();
+							setError();
 
-						const fullname = e.target.name.value.trim();
-						const nickname = fullname.split(" ")[0].trim();
-						const username = e.target.username.value.trim();
-						const password = e.target.password.value;
-						if (!fullname || !username || !password) return;
-						if (!recaptchaToken)
-							return setError("You need to complete the captcha first!");
+							const fullname = e.target.name.value.trim();
+							const nickname = fullname.split(" ")[0].trim();
+							const username = e.target.username.value.trim();
+							const password = e.target.password.value;
+							if (!fullname || !username || !password) return;
+							if (!recaptchaToken)
+								return setError("You need to complete the captcha first!");
 
-						// Request
-						setLoading(true);
-						axios
-							.post(`${process.env.NEXT_PUBLIC_APIURL}/register`, {
-								fullname,
-								nickname: nickname ? nickname : fullname,
-								username,
-								password,
-								recaptcha: recaptchaToken
-							})
-							.then(res => {
-								Cookies.set("sessionToken", res.data.token, {
-									domain:
-										location.host === config.domain ? config.domain : null,
-									expires: 365
+							// Request
+							setLoading(true);
+							axios
+								.post(`${process.env.NEXT_PUBLIC_APIURL}/register`, {
+									fullname,
+									nickname: nickname ? nickname : fullname,
+									username,
+									password,
+									recaptcha: recaptchaToken
+								})
+								.then(res => {
+									Cookies.set("sessionToken", res.data.token, {
+										domain:
+											location.host === config.domain ? config.domain : null,
+										expires: 365
+									});
+									Router.push("/");
+								})
+								.catch(err => {
+									if (err.response.data.err === "nameLength") {
+										setError(
+											`Your name must be between ${config.inputBounds.name.min} and ${config.inputBounds.name.max} characters`
+										);
+									} else if (err.response.data.err === "usernameLength") {
+										setError(
+											`Your username must be between ${config.inputBounds.username.min} and ${config.inputBounds.username.max} characters`
+										);
+									} else if (err.response.data.err === "passwordLength") {
+										setError(
+											`Your password must be between ${config.inputBounds.password.min} and ${config.inputBounds.password.max} characters`
+										);
+									} else if (err.response.data.err === "usernameChars") {
+										setError(
+											"Your username can only contain letters and numbers, sorry"
+										);
+									} else if (err.response.data.err === "alreadyExists") {
+										setError("Sorry, someone's already taken this username");
+									} else if (err.response.data.err === "recaptcha") {
+										setError(
+											"Something went wrong while checking that you're human. Try again."
+										);
+									} else {
+										setError("Uh oh. Something's gone wrong. Maybe try again?");
+									}
+									setLoading(false);
 								});
-								Router.push("/");
-							})
-							.catch(err => {
-								if (err.response.data.err === "nameLength") {
-									setError(
-										`Your name must be between ${config.inputBounds.name.min} and ${config.inputBounds.name.max} characters`
-									);
-								} else if (err.response.data.err === "usernameLength") {
-									setError(
-										`Your username must be between ${config.inputBounds.username.min} and ${config.inputBounds.username.max} characters`
-									);
-								} else if (err.response.data.err === "passwordLength") {
-									setError(
-										`Your password must be between ${config.inputBounds.password.min} and ${config.inputBounds.password.max} characters`
-									);
-								} else if (err.response.data.err === "usernameChars") {
-									setError(
-										"Your username can only contain letters and numbers, sorry"
-									);
-								} else if (err.response.data.err === "alreadyExists") {
-									setError("Sorry, someone's already taken this username");
-								} else if (err.response.data.err === "recaptcha") {
-									setError(
-										"Something went wrong while checking that you're human. Try again."
-									);
-								} else {
-									setError("Uh oh. Something's gone wrong. Maybe try again?");
-								}
-								setLoading(false);
-							});
-					}}
-				>
-					<Box.Header>Tell us a little about yourself</Box.Header>
-					<Box.Content>
-						<Input
-							label="Name"
-							name="name"
-							placeholder="Jessica Adams"
-							fluid
-							onChange={() => setError(false)}
-							maxLength={config.inputBounds.name.max}
-						/>
-
-						<Spacer y={0.5} />
-
-						<Input
-							label="Username"
-							name="username"
-							placeholder="jessica"
-							fluid
-							onChange={() => setError(false)}
-							maxLength={config.inputBounds.username.max}
-						/>
-
-						<Spacer y={0.5} />
-
-						<Input.Password
-							label="Password"
-							name="password"
-							placeholder="•••••••••••"
-							fluid
-							onChange={() => setError(false)}
-							maxLength={config.inputBounds.password.max}
-						/>
-
-						<Spacer y={1} />
-
-						<div className="recaptcha">
-							<Recaptcha
-								sitekey="6LdTDqQZAAAAABVHA5tveWNheITaG9rnAbFzdxVH"
-								onChange={token => setRecaptchaToken(token)}
-								onErrored={() =>
-									setError(
-										"Something went wrong while checking that you're human. Refresh the page to try again."
-									)
-								}
-								onExpired={() =>
-									setError(
-										"Something went wrong while checking that you're human. Refresh the page to try again."
-									)
-								}
+						}}
+					>
+						<Box.Header>Tell us a little about yourself</Box.Header>
+						<Box.Content>
+							<Input
+								label="Name"
+								name="name"
+								placeholder="Jessica Adams"
+								fluid
+								onChange={() => setError(false)}
+								maxLength={config.inputBounds.name.max}
 							/>
-						</div>
 
-						<Spacer y={1} />
+							<Spacer y={0.5} />
 
-						<Button loading={loading} fluid primary>
-							Register
-						</Button>
+							<Input
+								label="Username"
+								name="username"
+								placeholder="jessica"
+								fluid
+								onChange={() => setError(false)}
+								maxLength={config.inputBounds.username.max}
+							/>
 
-						<Spacer y={0.5} />
+							<Spacer y={0.5} />
 
-						{error ? <p className="error">{error}</p> : <></>}
-					</Box.Content>
-					<Box.Footer>
-						Psst! Make sure you take a look at our{" "}
-						<Link href="/docs/terms">
-							<a className="normal">Terms of Service</a>
-						</Link>{" "}
-						and{" "}
-						<Link href="/docs/privacy">
-							<a className="normal">Privacy Policy</a>
-						</Link>
-						. By signing up, you're indicating that you agree to them. Please
-						note that you are only allowed{" "}
-						<strong>one primary account per person</strong>, and violation of
-						this rule could result in consequences to all of your accounts.
-						You'll be able to create secondary accounts soon :)
-					</Box.Footer>
-				</Box>
+							<Input.Password
+								label="Password"
+								name="password"
+								placeholder="•••••••••••"
+								fluid
+								onChange={() => setError(false)}
+								maxLength={config.inputBounds.password.max}
+							/>
+
+							<Spacer y={1} />
+
+							<div className="recaptcha">
+								<Recaptcha
+									sitekey="6LdTDqQZAAAAABVHA5tveWNheITaG9rnAbFzdxVH"
+									onChange={token => setRecaptchaToken(token)}
+									onErrored={() =>
+										setError(
+											"Something went wrong while checking that you're human. Refresh the page to try again."
+										)
+									}
+									onExpired={() =>
+										setError(
+											"Something went wrong while checking that you're human. Refresh the page to try again."
+										)
+									}
+								/>
+							</div>
+
+							<Spacer y={1} />
+
+							<Button loading={loading} fluid primary>
+								Register
+							</Button>
+
+							<Spacer y={0.5} />
+
+							{error ? <p className="error">{error}</p> : <></>}
+						</Box.Content>
+						<Box.Footer>
+							Psst! Make sure you take a look at our{" "}
+							<Link href="/docs/terms">
+								<a className="normal">Terms of Service</a>
+							</Link>{" "}
+							and{" "}
+							<Link href="/docs/privacy">
+								<a className="normal">Privacy Policy</a>
+							</Link>
+							. By signing up, you're indicating that you agree to them. Please
+							note that you are only allowed{" "}
+							<strong>one primary account per person</strong>, and violation of
+							this rule could result in consequences to all of your accounts.
+							You'll be able to create secondary accounts soon :)
+						</Box.Footer>
+					</Box>
+				)}
 
 				<style jsx>{`
 					h2 {
