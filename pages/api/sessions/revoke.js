2159488d03f5d69c0a89e7b1916a03189ec5854d
Archie Baer <archie@abaer.dev>
2020-06-21 11:12:33 +0100

    Minor change

--- pages/api/sessions/revoke.js
+++ pages/api/sessions/revoke.js
@@ -2,7 +2,7 @@ import db from "../../../util/db";
 import sessionAuth from "../../../util/sessionAuth";
 
 export default async (req, res) => {
-	const {user, session} = await sessionAuth(req.headers.authorization);
+	const {user} = await sessionAuth(req.headers.authorization);
 	if (!user) return res.status(401).json({err: "badAuthorization"});
 	if (typeof req.query.id !== "string")
 		return res.status(400).json({err: "badRequest"});
