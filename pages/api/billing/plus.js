5de47f54b32053f2d05ceff951c45cbe72d9a25f
Archie Baer <archie@abaer.dev>
2020-06-23 18:26:10 +0100

    New subscription creation

---
+++ pages/api/billing/plus.js
@@ -0,0 +1,35 @@
+import sessionAuth from "../../../util/sessionAuth";
+import Stripe from "stripe";
+const stripe = Stripe(process.env.STRIPE_SK);
+
+export default async (req, res) => {
+	const {user} = await sessionAuth(req.headers.authorization);
+	if (!user) return res.status(401).json({err: "badAuthorization"});
+	if (!user.stripeCustomerId)
+		return res.status(400).json({err: "billing.unregistered"});
+	if (
+		!["monthly", "yearly", "monthly-max", "yearly-max"].includes(req.query.plan)
+	)
+		return res.status(400).json({err: "billing.invalidPlan"});
+
+	// Check for active subscription
+	const subscriptions = await stripe.subscriptions.list();
+	if (user.plus || subscriptions.data.length > 0)
+		return res.status(400).json({err: "alreadySet"});
+
+	// Create Subscription
+	await stripe.subscriptions.create({
+		customer: user.stripeCustomerId,
+		items: [
+			{
+				price:
+					process.env[
+						`STRIPE_PLUS_${req.query.plan.toUpperCase().replace("-", "_")}`
+					]
+			}
+		]
+	});
+
+	// Response
+	res.json({});
+};
